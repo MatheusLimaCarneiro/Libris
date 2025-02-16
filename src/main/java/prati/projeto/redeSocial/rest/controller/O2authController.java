165bbe198a6dafce672aec3c57fc755e3342445e
Matheus Lima <matheuslimacarneirors@gmail.com>
2025-02-16 16:05:11 -0300

    feat: implementa autenticação OAuth2, adiciona CustomOAuth2User, OAuth2Controller e CustomOAuth2UserService
    Este commit implementa a autenticação OAuth2, adicionando novas classes e ajustando classes existentes para suportar a autenticação via provedores externos.
    
    - Implementa autenticação OAuth2:
      - Adiciona a classe `CustomOAuth2User` para gerenciar detalhes do usuário OAuth2.
      - Cria o `OAuth2Controller` para lidar com endpoints de autenticação OAuth2, como login e callback.
      - Implementa o `CustomOAuth2UserService` para processar informações do usuário OAuth2 e integrar com o sistema de gerenciamento de usuários existente.
    
    - Ajustes em classes existentes:
      - Modifica a classe `Usuario` para suportar atributos necessários para a autenticação OAuth2.
      - Atualiza a classe `SecurityConfig` para configurar a integração do OAuth2 no fluxo de segurança da aplicação.
      - Adiciona métodos no `JwtService` para gerar tokens JWT com base no OAuth2:
        - `gerarTokenO2auth(CustomOAuth2User oauth2User)`: Gera um token JWT para usuários autenticados via OAuth2.
        - `gerarRefreshTokenO2auth(CustomOAuth2User oauth2User)`: Gera um refresh token JWT para usuários autenticados via OAuth2.
    
    - Ajustes nos controllers:
      - Realiza melhorias nos controllers para garantir compatibilidade com a autenticação OAuth2.

---
+++ src/main/java/prati/projeto/redeSocial/rest/controller/O2authController.java
@@ -0,0 +1,50 @@
+package prati.projeto.redeSocial.rest.controller;
+
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import prati.projeto.redeSocial.rest.dto.TokenDTO;
+import prati.projeto.redeSocial.rest.response.ServiceResponse;
+import prati.projeto.redeSocial.security.JwtService;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/libris/auth")
+public class O2authController {
+
+    private final JwtService jwtService;
+    private static final Logger logger = LoggerFactory.getLogger(O2authController.class);
+
+    @GetMapping("/oauth2/success")
+    @ResponseStatus(HttpStatus.OK)
+    public ServiceResponse<TokenDTO> oauth2Success( @RequestParam String token,
+                                                    @RequestParam String refreshToken) {
+        logger.info("Token recebido no endpoint /oauth2/success: " + token);
+
+        if (token == null || token.isEmpty()) {
+            logger.error("Token ausente ou vazio.");
+            return new ServiceResponse<>(null, "Token ausente ou vazio", false, getFormattedTimestamp());
+        }
+
+        if (!jwtService.tokenValido(token)) {
+            logger.error("Token inválido: " + token);
+            return new ServiceResponse<>(null, "Token inválido", false, getFormattedTimestamp());
+        }
+
+        TokenDTO tokenDTO = new TokenDTO();
+        tokenDTO.setToken(token);
+        tokenDTO.setRefreshToken(refreshToken);
+        tokenDTO.setLogin(jwtService.obterLoginUsuario(token));
+        return new ServiceResponse<>(tokenDTO, "Login com OAuth2 bem-sucedido", true, getFormattedTimestamp());
+    }
+
+    private String getFormattedTimestamp() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return LocalDateTime.now().format(formatter);
+    }
+}
---
+++ src/main/java/prati/projeto/redeSocial/service/CustomOAuth2UserService.java
@@ -0,0 +1,46 @@
+package prati.projeto.redeSocial.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import prati.projeto.redeSocial.modal.entity.CustomOAuth2User;
+import prati.projeto.redeSocial.modal.entity.Usuario;
+import prati.projeto.redeSocial.repository.UsuarioRepository;
+
+@AllArgsConstructor
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService{
+
+    private final UsuarioService usuarioService;
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oauth2User = super.loadUser(userRequest);
+
+        String email = oauth2User.getAttribute("email");
+        String name = oauth2User.getAttribute("name");
+
+        Usuario usuario = usuarioRepository.findByEmail(email).orElseGet(() -> {
+            Usuario novoUsuario = new Usuario();
+            novoUsuario.setEmail(email);
+            novoUsuario.setUsername(generateUsername(name, email));
+            novoUsuario.setSenha(null);
+            novoUsuario.setAuthProvider("google");
+            return usuarioService.saveUsuario(novoUsuario);
+        });
+
+        return new CustomOAuth2User(usuario);
+    }
+
+    private String generateUsername(String name, String email) {
+        String username = name != null ? name : email.split("@")[0];
+        if (usuarioRepository.existsByUsername(username)) {
+            username = username + "_" + System.currentTimeMillis();
+        }
+        return username;
+    }
+}
