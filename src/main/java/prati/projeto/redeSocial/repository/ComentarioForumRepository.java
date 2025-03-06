e5d31969a4675aff7242f75600a10f74c0e0d64b
Matheus Lima <matheuslimacarneirors@gmail.com>
2025-03-05 22:52:33 -0300

    feat: implementa função de listar comentários por username no ComentarioController e ComentarioForumController
    Este commit implementa a funcionalidade de listar comentários com base no username do usuário nos controllers de Comentario e ComentarioForum. Foram feitas mudanças no ComentarioForumRepository, ComentarioRepository, ComentarioForumServiceImpl, ComentarioServiceImpl, ComentarioForumService, ComentarioService e no método listarComentariosPorUsername para garantir que os comentários sejam filtrados corretamente pelo username e, no caso do ComentarioForum, também pelo postId.
    
    - Melhorias no ComentarioForum:
      - Implementa o método `listarComentariosPorUsername` no `ComentarioForumServiceImpl` para filtrar comentários pelo username e postId.
      - Atualiza o `ComentarioForumController` para incluir o endpoint `/libris/posts/{postId}/comentarios/usuario/{username}`.
    
    - Melhorias no Comentario:
      - Implementa o método `listarComentariosPorUsername` no `ComentarioServiceImpl` para filtrar comentários pelo username.
      - Atualiza o `ComentarioController` para incluir o endpoint `/libris/comentarios/listar/usuario/{username}`.
    
    - Ajustes gerais:
      - Adiciona validações para garantir que o usuário e o perfil existam antes de buscar os comentários.
    
    - Impacto:
      - Agora é possível listar os comentários de um usuário específico tanto no fórum quanto nos comentários de livros.
      - A paginação foi mantida para melhorar o desempenho e a experiência do usuário.

--- src/main/java/prati/projeto/redeSocial/repository/ComentarioForumRepository.java
+++ src/main/java/prati/projeto/redeSocial/repository/ComentarioForumRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import prati.projeto.redeSocial.modal.entity.ComentarioForum;
+import prati.projeto.redeSocial.modal.entity.Perfil;
 
 
 public interface ComentarioForumRepository extends JpaRepository<ComentarioForum, Integer> {
     Page<ComentarioForum> findByPostForumId(Integer comentarioId, Pageable pageable);
+    Page<ComentarioForum> findByPerfil(Perfil perfil, Pageable pageable);
 }
